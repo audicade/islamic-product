82065adf87fd9a64304bf40c0db98528531c79ff
juukyokai <juukyokai00@gmail.com>
2021-04-15 01:12:06 +0700

    fixing landing order

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,7 +6,7 @@ class Main extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('templates/header');
-		$this->load->view('landing');
+		$this->load->view('landing_bisnis');
 		$this->load->view('templates/footer');
 	}
 }
